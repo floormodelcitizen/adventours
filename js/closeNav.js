7001430c76ec1bb17a6155446c59f0d39859d063
Chad Christopher <devluxv20@gmail.com>
2022-06-20 20:31:12 -0700

    Add scss site

---
+++ js/closeNav.js
@@ -0,0 +1,9 @@
+/* Open */
+// function openNav() {
+//     document.getElementById("myNav").style.display = "block";
+//   }
+  
+  /* Close */
+  function closeNav() {
+    document.getElementById("navi-toggle").style.display = "none";
+  }
